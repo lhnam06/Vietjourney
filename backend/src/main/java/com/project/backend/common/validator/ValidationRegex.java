d0a3f6b85bed140a02a0942b6f1f296ce87f09ae
Youmei <trantanphat.stu@gmail.com>
2026-05-29 22:02:13 +0700

    feat: loosen auth constraints manually applied to deploy branch

--- backend/src/main/java/com/project/backend/common/validator/ValidationRegex.java
+++ backend/src/main/java/com/project/backend/common/validator/ValidationRegex.java
@@ -3,10 +3,8 @@ package com.project.backend.common.validator;
 public class ValidationRegex {
     public static final String NO_SPACE = "^\\S+$";
     public static final String HAS_DIGIT = "^.*\\d.*";
-    public static final String HAS_UPPERCASE = "^.*[A-Z].*";
-    public static final String HAS_LOWERCASE = "^.*[a-z].*";
-    public static final String HAS_SYMBOL = ".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?].*";
     public static final String ONLY_KEYBOARD_CHARS = "^[\\x21-\\x7E]+$"; // prevent icons, Unicode chars
+    public static final String HAS_LETTER = "^.*[a-zA-Z].*";
 
     private ValidationRegex(){}; // private constructor to prevent object creation
 }
